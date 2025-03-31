695e82b6ac3eb5839b833a20874914fe80b67560
Jayaprakash Panabaka <123289779+JayaprakashPanabaka@users.noreply.github.com>
2025-03-31 09:45:46 +0000

    first

--- src/Module 3/Introduction to ReactJS/Ques_1.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_1.jsx
@@ -1,4 +1,14 @@
 // JSX Syntax Basics
 // Description: Write basic JSX that displays "Hello, React!"
 
+function Hello() {
+    return(
+        <div>
+            <h1>Hello World...!</h1>
+        </div>
+    )
+}
+
+export default Hello;
+
 
